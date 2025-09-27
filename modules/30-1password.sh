e2b051fa227c2194ce2ebcfc2db1dd5b51aa694e
Owen D'Aprile <git@owen.sh>
2025-09-26 20:41:04 -0400

    fix: Syntax issues in tmpfiles config

--- modules/30-1password.sh
+++ modules/30-1password.sh
@@ -5,4 +5,4 @@ set -euxo pipefail
 chmod +x /tmp/1password.sh
 /tmp/1password.sh
 rm --force --recursive --verbose /tmp/1password.sh
-sed --in-place "s|^L|^+|g" /usr/lib/tmpfiles.d/onepassword.conf
+sed --in-place "s/^L/L+/g" /usr/lib/tmpfiles.d/onepassword.conf
